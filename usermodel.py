731e6b647df8e3943740d8b67581d2889d34b5fa
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2020-09-26 16:38:10 -0400

    testing

---
+++ usermodel.py
@@ -0,0 +1,15 @@
+class User(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String(80), unique=True, nullable=False)
+   password = db.Column(db.String(20), nullable=False)
+   email = db.Column(db.String(100), unique=True, nullable=False)
+
+   def __init__(self, username, password, email):
+       self.username = username
+       self.password = password
+       self.email = email
+       self.country= country
+       self.city = city
+
+   def __repr__(self):
+       return '<User %r>' % self.username
