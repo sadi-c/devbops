337e9ecf72ef3cd7ebfbec02d4c8f86cfac6c8d9
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2020-09-26 16:20:39 -0400

    user service v1

---
+++ app.py
@@ -0,0 +1,17 @@
+Import flask
+
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+   username = request.form['username']
+   password = request.form['password']
+   email = request.form['email']
+   country = request.form['country']
+   city = request.form['city']
+   user = User(username=username, password=password, email=email, country=country, city=city)
+   db.session.add(user)
+   db.session.commit()
+   return jsonify({
+       'response': 'User ' + username + ' created successfully'
+   })
--- user_service.py
+++ user_service.py
@@ -1,10 +1,10 @@
-class Task(db.Model):
+class Service(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    content = db.Column(db.Text)
    add_date = db.Column(db.DateTime, default=datetime.datetime.now())
    end_date = db.Column(db.DateTime)
    done = db.Column(db.Boolean, default=False)
-   user = db.relationship('User', backref='tasks', lazy=True)
+   user = db.relationship('User', backref='service', lazy=True)
    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
    def __init__(self, content, user):
