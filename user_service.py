755af1c37590f9496e39be3ca3d4f0e3097fbf4f
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2020-09-26 16:06:35 -0400

    user  service v1

---
+++ user_service.py
@@ -0,0 +1,15 @@
+class Task(db.Model):
+   id = db.Column(db.Integer, primary_key=True)
+   content = db.Column(db.Text)
+   add_date = db.Column(db.DateTime, default=datetime.datetime.now())
+   end_date = db.Column(db.DateTime)
+   done = db.Column(db.Boolean, default=False)
+   user = db.relationship('User', backref='tasks', lazy=True)
+   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+   def __init__(self, content, user):
+       self.content = content
+       self.user = user
+
+   def __repr__(self):
+       return '<Task %r>' % self.content
